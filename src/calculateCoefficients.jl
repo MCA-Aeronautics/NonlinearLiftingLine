d93cd776f2b9b1ac784ec2f7fa7d78e6795f00f0
Mark C. Anderson <anderson.mark.az@gmail.com>
2020-05-21 10:47:42 -0700

    Testing to see if airfoil data exist

--- src/calculateCoefficients.jl
+++ src/calculateCoefficients.jl
@@ -10,12 +10,22 @@ function calculateCoefficients(chordwiseCoordinates,thicknessCoordinates, angle,
 
     angle = angle*180/pi # Converting to degrees
 
+    airfoilName = "NACA0010"
+
+    roundedReynoldsNumber = round(reynoldsNumber/100000) * 100000
+
+    filename = string("airfoil-data/",airfoilName,"_",roundedReynoldsNumber,".csv")
+
+    if isfile(filename)
+        println(filename," exists!")
+    else
+        println(filename," does not exist...")
+    end
+
+    # The old way: each airfoil was calculated on each iteration
     Xfoil.setCoordinates(chordwiseCoordinates,thicknessCoordinates)
-
     cl, cdd, cdp, cm, converged = Xfoil.solveAlpha(angle,reynoldsNumber)
-
     #println("Angle = ",angle,", Reynolds Number = ",reynoldsNumber," -> cl = ",cl)
-
-    return cl[1], cdd[1], cdp[1], cm[1], converged[1]
+    # return cl[1], cdd[1], cdp[1], cm[1], converged[1]
 
 end
