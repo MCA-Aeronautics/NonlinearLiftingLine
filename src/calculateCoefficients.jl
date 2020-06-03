7c0e6987885756689f2d4cedf8e2ca721eb89d0c
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-06-03 12:45:57 -0600

    cl is now calculated with a 2D airfoil data spline

--- src/calculateCoefficients.jl
+++ src/calculateCoefficients.jl
@@ -2,7 +2,7 @@
 
 # include("Desktop/FLOW Lab/Strip Theory/calculateCoefficients.jl")
 
-Pkg.add(PackageSpec(path="/Users/markanderson/Box/FLOW-MCA/FLOW-Code/Repositories/personal-projects/makeAirfoil"))
+Pkg.develop(PackageSpec(path="/Users/markanderson/Box/FLOW-MCA/FLOW-Code/Repositories/personal-projects/makeAirfoil"))
 Pkg.add("FLOWMath")
 Pkg.add("CSV")
 import makeAirfoil.tabulateData
@@ -15,26 +15,27 @@ function calculateCoefficients(chordwiseCoordinates,thicknessCoordinates, angle,
 
     airfoilName = "NACA0010"
 
-    roundedReynoldsNumber = round(reynoldsNumber/100000) * 100000
+    filename = string("airfoil-data/",airfoilName,".csv")
 
-    filename = string("airfoil-data/",airfoilName,"_",roundedReynoldsNumber,".csv")
-
-    if isfile(filename)
-        # println(filename," exists!")
-        # Then do interpolation
-        data = CSV.read(filename)
-        cl = FLOWMath.linear(data[:,1],data[:,2],angle)
-    else
+    if isfile(filename) == false
         println(filename," does not exist...")
         println("Let's create that file real fast")
         airfoil = zeros(length(chordwiseCoordinates),2)
         airfoil[:,1] = chordwiseCoordinates
         airfoil[:,2] = thicknessCoordinates
-        angleRange = -20:0.2:30
+        angleRange = -20:1:30
+        reynoldsNumberRange = 1e5:1e5:1e6
         path = "airfoil-data"
         airfoilName = "NACA0010"
-        tabulateData(airfoil,angleRange,path,airfoilName,roundedReynoldsNumber)
+        tabulateData(airfoil,angleRange,reynoldsNumberRange,path,airfoilName)
     end
+
+    # Do the 2D interpolation
+    data = CSV.read(filename)
+    data_angles = convert(Array,data[2:end,1])
+    data_reynolds = convert(Array,data[1,2:end])
+    data_cl = convert(Array,data[2:end,2:end])
+    cl = FLOWMath.interp2d(akima,data_angles,data_reynolds,data_cl,angle:angle,reynoldsNumber:reynoldsNumber)
 
     # The old way: each airfoil was calculated on each iteration
     # Xfoil.setCoordinates(chordwiseCoordinates,thicknessCoordinates)
