c57a6e8621b9a6d12b411055ab2394a9143590ed
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-06-08 13:13:13 -0600

    Removed effective angle print statement

--- src/NonlinearLiftingLine.jl
+++ src/NonlinearLiftingLine.jl
@@ -94,10 +94,8 @@ module NonlinearLiftingLine
 
                 # Calculate the lift and drag coefficients for that angle for each airfoil
                 # cl[j], cdd[j], cdp[j], cm[j], converged[j] = calculateCoefficients(airfoil[:,1],airfoil[:,2], effectiveAOA[j], localReynoldsNumber/chord);
-                cl[j] = calculateCoefficients(airfoil[:,1],airfoil[:,2], effectiveAOA[j], localReynoldsNumber/chord);
-                # cl[j] = 2*pi*effectiveAOA[j]
-                println("Effective AOA = ",effectiveAOA[j],"    Local Re = ",localReynoldsNumber/chord,"  cl = ",cl[j])
-
+                cl[j] = calculateCefficients(airfoil[:,1],airfoil[:,2], effectiveAOA[j], localReynoldsNumber/chord);
+                
                 # Use the coefficients to calculate the circulation about each airfoil. Not sure which one to use
                 circulation = 0.5 * freestream[j] * cl[j] * chord
 
