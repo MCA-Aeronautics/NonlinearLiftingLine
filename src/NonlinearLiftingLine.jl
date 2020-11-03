c48cfced2e1719f8ed73d8235ccc9c2bde085531
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-11-03 14:19:46 -0700

    Convenience with blown wing

--- src/NonlinearLiftingLine.jl
+++ src/NonlinearLiftingLine.jl
@@ -10,13 +10,13 @@ module NonlinearLiftingLine
 
     # Adding all the additional packages that are used in this code
     using Pkg
-    Pkg.add("PyPlot")
-    Pkg.add("LinearAlgebra")
-    Pkg.add("FLOWMath")
-    Pkg.add("CSV")
-    Pkg.develop(PackageSpec(url="https://github.com/byuflowlab/Xfoil.jl"))
-    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/VLMMCA"))
-    Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/makeAirfoil"))
+    # Pkg.add("PyPlot")
+    # Pkg.add("LinearAlgebra")
+    # Pkg.add("FLOWMath")
+    # Pkg.add("CSV")
+    # Pkg.develop(PackageSpec(url="https://github.com/byuflowlab/Xfoil.jl"))
+    # Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/VLMMCA"))
+    # Pkg.develop(PackageSpec(url="https://github.com/Mark-C-Anderson/makeAirfoil"))
     using PyPlot
     using LinearAlgebra
     using FLOWMath
@@ -50,10 +50,11 @@ module NonlinearLiftingLine
                  airfoil,
                  airfoilName,
                  freestream,
+                 rbfs,
                  density = 1.225)
 
         # Defining some variables
-        maxIter = 50
+        maxIter = 100
         showConvergence = true
         numPanels = length(panels[:,1])
         nu = 1.48e-5;
@@ -84,92 +85,6 @@ module NonlinearLiftingLine
             chord_lhs = panels[i,10] - panels[i,1]
             chord_rhs = panels[i,7] - panels[i,4]
             chord[i] = (chord_lhs + chord_rhs) / 2
-        end
-
-        # Some of Eduardo's code
-        database_path = "/Users/markanderson/Box/FLOW-MCA/Code/NonlinearLiftingLine/airfoil-data/eduardo";
-        db  = jdb.loadtable(joinpath(database_path, "index.csv"))
-
-        # Singular kernel
-        zeta_sing(r) = r==0 ? 1 : 0
-
-        # erf Gaussian kernel
-        zeta_gauserf(r) = 1/(2*pi)^(3/2) * exp(-r^2/2)
-
-        # Gaussian kernel
-        zeta_gaus(r) = 3/(4*pi)*exp(-r^3)
-
-        # Winckelmans algebraic kernel
-        zeta_wnklmns(r) = 7.5 / (4*pi) / (r^2 + 1)^3.5
-
-        #--MAGIC CODE--#
-
-        # Generate RBF interpolation function
-        rbf_axes = [:alpha, :re, :ma, :ncrit]    # Axes of the RBF
-
-
-        # Read all files in database
-        Xps = Dict{Symbol, Array{Array{Float64, 1}, 1}}()
-        vals = Dict{Symbol, Array{Float64, 1}}()
-        rbfs = Dict{Symbol, Function}()
-
-        # This section just extracts all of the data from the files and feeds it into the generate_RBF() function
-
-        for file in [:clfile, :cdfile, :cmfile]  # Iterate over each file
-            
-            Xp = Array{Float64, 1}[]
-            val = Float64[]
-            
-            # Convert file symbol to column header
-            colheader = Symbol(adb.FIELD2_HEADER[file])
-            
-            Xmin = [Inf for xi in 1:length(rbf_axes)]
-            Xmax = [-Inf for xi in 1:length(rbf_axes)]
-            valmin, valmax = Inf, -Inf
-            
-            for (rowi, row) in enumerate(db) # Iterate over each row
-
-                filename = row[colheader]   # File to read
-
-                # Read data in file
-                data = CSV.read(joinpath(database_path, adb.DIRS[file], filename))
-                
-                for drow in eachrow(data) # Iterate over rows in the data
-                    this_Xp = Float64[ax != :alpha ? row[Symbol(adb.FIELD2_HEADER[ax])] : drow[1] for ax in rbf_axes]
-                    this_val = drow[2]
-                    push!(Xp, this_Xp)
-                    push!(val, this_val)
-                    
-                    for xi in 1:length(rbf_axes)
-                        this_Xp[xi] < Xmin[xi] ? Xmin[xi]=this_Xp[xi] : 
-                        this_Xp[xi] > Xmax[xi] ? Xmax[xi]=this_Xp[xi] :
-                                                nothing
-                    end
-                    this_val < valmin ? valmin = this_val : this_val > valmax ? valmax = this_val : nothing
-                end
-                
-            end
-            
-            
-            println("Generating $file RBF function with $(length(Xp)) data points...")
-            
-            # Scale each variable in the range 0 to 1
-            X_scaling = [x == 0 ? 1 : x for x in Xmax .- Xmin]
-            Xp_scaled = [(X .- Xmin) ./ X_scaling for X in Xp]
-            val_scaled = (val .- valmin) ./ (valmax - valmin)
-            
-            # Generate RBF interpolation function
-            rbf, A = generate_RBF(Xp, val; zeta=zeta_gaus, sigmas=0.1)
-            
-            # Generate scaled RBF interpolation function
-        #     rbf_scaled, A = generate_RBF(Xp_scaled, val_scaled; zeta=zeta_gaus, sigmas=1.50)
-            rbf_scaled, A = generate_RBF(Xp_scaled, val_scaled; zeta=zeta_gaus, sigmas=5.0)
-            rbf(X) = valmin + (valmax - valmin)*rbf_scaled( (X.-Xmin)./ X_scaling )
-            
-            
-            Xps[file] = Xp
-            vals[file] = val
-            rbfs[file] = rbf
         end
 
         # Now we will create a loop that will converge to a circulation value that will be available
@@ -261,7 +176,7 @@ module NonlinearLiftingLine
                 grid("on")
                 legend()
                 #xlim(-1.3,1.3)
-                ylim(minimum(cl_VLM)*0.5,maximum(cl_VLM)*1.25)
+                ylim(minimum(cl)*0.5,maximum(cl)*1.25)
                 draw()
                 println("Iteration: ",i,"\t CL: ",round(CL,digits=6),"\t RMS Difference: ",round(cl_difference_rms,digits=6))
 
@@ -277,13 +192,12 @@ module NonlinearLiftingLine
         end
 
         # Getting the results from the nonlinear solver
-        #CL, cL, _ = calculateLift(density,freestream,panels,GammaValues); # Lift
+        CL, cL, _ = calculateLift(density,freestream,panels,GammaValues); # Lift
         CDi_near, _, _ = calculateInducedDrag(density,freestream,panels,GammaValues,cl); # Near-field induced drag
 
         CL_VLM, cl_VLM, cLSpanLocations_VLM = calculateLift(density,freestream,panels,GammaValues_VLM); # Lift
 
-        CL = trapz(cLSpanLocations_VLM,cl)
-        println("NLL CL = $CL")
+        #CL = trapz(cLSpanLocations_VLM,cl)
 
         return CL, CDi_near, cl, cLSpanLocations_VLM
 
@@ -334,42 +248,6 @@ module NonlinearLiftingLine
     
         return effectiveAlphas
     
-    end
-
-    # Returns a radial basis function interpolation of a field
-    # with values `val` at positions `Xp`. `zeta` is the chosen
-    # basis function
-    function generate_RBF(Xp, val; zeta=zeta_gaus, sigmas=0.1)
-        
-        # ERROR CASES
-        if size(Xp,1)!=size(val,1)
-            error("size(Xp,1)!=size(val,1)")
-        end
-        
-        Np = size(Xp, 1)                     # Number of data points
-        
-        if size(sigmas)==()
-            sgms = sigmas*ones(Np)           # Spreading of every basis function
-        else
-            sgms = sigmas
-        end
-            
-        # j-th scaled basis evaluated at X
-        zetasgm(j, X) = zeta(Statistics.norm(X-Xp[j])/sgms[j])/sgms[j]^3
-        
-        # Matrix with basis functions evaluated at every point
-        # Z[i,j] corresponds to the j-th basis evaluated at i-th point
-        Z = [zetasgm(j, Xi) for Xi in Xp, j in 1:Np]
-        
-        # Solves for the alpha coefficients of every basis
-        # A = Z\val
-        # A = LinearAlgebra.pinv(Z)*val
-        A = IterativeSolvers.gmres(Z, val)
-        
-        # Generates RBF interpolation function
-        rbf(X) = sum([A[j]*zetasgm(j, X) for j in 1:Np])
-        
-        return rbf, A
     end
 
 end
